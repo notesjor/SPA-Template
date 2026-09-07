96a6db3cdf2c98ee8663ebed2533913a4ebfed3a
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2026-09-07 17:05:45 +0200

    // mdi

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import { createPinia } from 'pinia';
 
 // Vuetify Setup
 import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+	},
+});
 const pinia = createPinia();
 
 createApp(App).use(pinia).use(vuetify).mount('#app');
